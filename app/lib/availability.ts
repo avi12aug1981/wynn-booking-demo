cde41b336e2d58ebbd3e655a9ca0c5b41a93b7e3
avi12aug1981 <avadesh12chauhan@gmail.com>
2026-05-29 16:51:33 -0700

    Add room availability helper

---
+++ app/lib/availability.ts
@@ -0,0 +1,24 @@
+import { BookingStatus } from "@prisma/client";
+import { prisma } from "./prisma";
+
+export function calculateNumberOfNights(checkInDate: Date, checkOutDate: Date): number {
+  const millisecondsPerDay = 1000 * 60 * 60 * 24;
+  return Math.ceil((checkOutDate.getTime() - checkInDate.getTime()) / millisecondsPerDay);
+}
+
+export async function isRoomAvailable(roomId: number, checkInDate: Date, checkOutDate: Date): Promise<boolean> {
+  const overlappingBooking = await prisma.booking.findFirst({
+    where: {
+      roomId,
+      status: BookingStatus.CONFIRMED,
+      checkInDate: {
+        lt: checkOutDate,
+      },
+      checkOutDate: {
+        gt: checkInDate,
+      },
+    },
+  });
+
+  return overlappingBooking === null;
+}
